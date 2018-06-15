47eb33ebefbf03fc78d38f48fd427bfb6f63337f
Seweryn Wawrzynowicz <seweryn.wawrzynowicz@gmail.com>
2018-06-15 12:47:04 +0200

    base servises

--- PrzykladowyProjekt/Startup.cs
+++ PrzykladowyProjekt/Startup.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MistrzowieWynajmu.Models.Database;
 
 namespace PrzykladowyProjekt
 {
@@ -23,6 +25,9 @@ namespace PrzykladowyProjekt
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            var dbConnectionString = @"Server=(localdb)\mssqllocaldb;Database=MistrzowieDB;TrustedConnection=True;";
+            services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(dbConnectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
